36d98cfb0f3f1da1c212c4f7df7800f38510e331
Ganesh <paudelganesh080@gmail.com>
2025-12-24 11:40:45 -0500

    Added Navigation with Exception for not found with an special +not-found file.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>Home Screen a</Text>
+
+      <Link href="/Account" style = {styles.button}>
+        Go to Account a
+      </Link>
     </View>
   );
 }
@@ -18,5 +23,10 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontSize: 24,
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: "#fff",
   },
 });
